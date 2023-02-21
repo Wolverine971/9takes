4df951659e794026fb6c50bf0adacbfc45e57e5e
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2023-02-21 14:06:49 -0500

    goog ana

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { env } from '$env/dynamic/public';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'MEASUREMENT_ID', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src={`https://www.googletagmanager.com/gtag/js?id=${env.GOOGLE}`}>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', env.GOOGLE);
+	</script>
+</svelte:head>
