3c85dac40794efb21048fc280ddc4179a524bc5f
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2023-03-18 15:50:12 -0400

    remove scss

--- src/lib/components/blog/layout.svelte
+++ src/lib/components/blog/layout.svelte
@@ -1,23 +1,3 @@
 <div class="blog">
 	<slot />
 </div>
-
-<style lang="scss">
-	// .blog :global(a) {
-
-	// 	color: #260958;
-	// 	display: inline-flex;
-	// 	flex-wrap: wrap;
-	// 	overflow: hidden;
-	// }
-	// .blog :global(a::after) {
-	// 	content: '';
-	// 	background-image: url('/svgs/arrow.svg');
-	// 	display: inline-flex;
-	// 	justify-content: flex-start;
-	// 	align-items: center;
-	// 	width: 1em;
-	// 	height: 1em;
-	// 	background-size: 1em 1em;
-	// }
-</style>
