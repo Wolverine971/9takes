87ee8ea5e5a206bc560319d991f2425d77c25cc6
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2023-04-19 11:34:45 -0400

    gen sitemap test

---
+++ src/routes/poopmap/+server.js
@@ -0,0 +1,90 @@
+const SITE_URL = '9takes.com';
+const getAllPosts = async () => {
+	// const imports = import.meta.glob('/posts/**/*.md'); // make sure you get files from the right place
+	const community = import.meta.glob(`/src/blog/community/*.{md,svx,svelte.md}`);
+	const enneagram = import.meta.glob(`/src/blog/enneagram/*.{md,svx,svelte.md}`);
+	const generational = import.meta.glob(`/src/blog/generational/*.{md,svx,svelte.md}`);
+	const historical = import.meta.glob(`/src/blog/historical/*.{md,svx,svelte.md}`);
+	const lifesituations = import.meta.glob(`/src/blog/life-situations/*.{md,svx,svelte.md}`);
+	const people = import.meta.glob(`/src/blog/people/*.{md,svx,svelte.md}`);
+	const popculture = import.meta.glob(`/src/blog/pop-culture/*.{md,svx,svelte.md}`);
+	const situational = import.meta.glob(`/src/blog/situational/*.{md,svx,svelte.md}`);
+	const topical = import.meta.glob(`/src/blog/topical/*.{md,svx,svelte.md}`);
+
+	const imports = [
+		community,
+		enneagram,
+		generational,
+		historical,
+		lifesituations,
+		people,
+		popculture,
+		situational,
+		topical
+	];
+
+	let body = [];
+
+	for (const category in imports) {
+		for (const path in imports[category]) {
+			body.push(
+				imports[category][path]().then(({ metadata }) => {
+					return {
+						...metadata, // may not be required for sitemap
+						path
+					};
+				})
+			);
+		}
+	}
+	const posts = await Promise.all(body);
+
+	return posts;
+};
+
+export async function GET() {
+	const posts = await getAllPosts();
+
+	return new Response(
+		`
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <urlset
+      xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xhtml="https://www.w3.org/1999/xhtml"
+      xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+      xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+      xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+      xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+    >
+
+      <!-- this is where all the urls go -->
+       <url>
+        <loc>https://domain.tld</loc> <!-- homepage -->
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+      </url>
+
+      ${posts
+				.map(
+					(post) =>
+						`
+      <url>
+        <loc>${post.loc}</loc>
+        <lastmod>${post.lastmod && new Date(post.lastmod).toISOString()}</lastmod>
+        <changefreq>${post.changefreq}</changefreq>
+        <priority>0.7</priority>
+      </url>
+      `
+				)
+				.join('')}
+
+    <!-- this is where all the urls go -->
+
+    </urlset>`.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		}
+	);
+}
