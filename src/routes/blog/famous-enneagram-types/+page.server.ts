a7a4ba2643d41ef168b2c761ff91f8f412674eb5
DJ Wayne <djwayne35@gmail.com>
2023-05-07 23:50:29 -0400

    +page.ts

--- src/routes/blog/famous-enneagram-types/+page.server.ts
+++ src/routes/blog/famous-enneagram-types/+page.server.ts
@@ -1,25 +1,25 @@
-import type { PageServerLoad } from './$types';
-import { slugFromPath } from '$lib/slugFromPath';
+// import type { PageServerLoad } from './$types';
+// import { slugFromPath } from '$lib/slugFromPath';
 
-const MAX_POSTS = 20;
+// const MAX_POSTS = 20;
 
-export const load: PageServerLoad = async ({ url }) => {
-	const modules = import.meta.glob(`/src/blog/people/*.{md,svx,svelte.md}`);
+// export const load: PageServerLoad = async ({ url }) => {
+// 	const modules = import.meta.glob(`/src/blog/people/*.{md,svx,svelte.md}`);
 
-	const postPromises = Object.entries(modules).map(([path, resolver]) =>
-		resolver().then(
-			(post) =>
-				({
-					slug: slugFromPath(path),
-					...(post as unknown as App.MdsvexFile).metadata
-				} as App.BlogPost)
-		)
-	);
+// 	const postPromises = Object.entries(modules).map(([path, resolver]) =>
+// 		resolver().then(
+// 			(post) =>
+// 				({
+// 					slug: slugFromPath(path),
+// 					...(post as unknown as App.MdsvexFile).metadata
+// 				} as App.BlogPost)
+// 		)
+// 	);
 
-	const posts = await Promise.all(postPromises);
-	const publishedPosts = posts.filter((post) => post.published); //.slice(0, MAX_POSTS);
+// 	const posts = await Promise.all(postPromises);
+// 	const publishedPosts = posts.filter((post) => post.published); //.slice(0, MAX_POSTS);
 
-	publishedPosts.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
+// 	publishedPosts.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
 
-	return { people: publishedPosts };
-};
+// 	return { people: publishedPosts };
+// };
