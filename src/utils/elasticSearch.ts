cfdd0bea97d30f9d466a8dc6dff591fe16063af1
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2024-01-07 23:40:18 -0500

    updates

--- src/utils/elasticSearch.ts
+++ src/utils/elasticSearch.ts
@@ -1,4 +1,4 @@
-export const typeaheadQuery = ({ index, field, text, size }: { index: string, field: string, text: string, size: number = 10 }) => {
+export const typeaheadQuery = ({ index, field, text, size = 10 }: { index: string, field: string, text: string, size: number }) => {
 	return {
 		index,
 		body: {
