72861a370b6f20e09e22027df9901b99fb17aa9c
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2024-07-21 02:40:16 -0400

    formatting fix

--- src/routes/questionPrint/+page.server.ts
+++ src/routes/questionPrint/+page.server.ts
@@ -1,18 +1,17 @@
-
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-    try {
-        const session = event.locals.session;
+	try {
+		const session = event.locals.session;
 
-        return {
-            session,
-        };
-    } catch (e) {
-        console.log(e);
-        throw error(500, {
-            message: 'Error finding questions'
-        });
-    }
+		return {
+			session
+		};
+	} catch (e) {
+		console.log(e);
+		throw error(500, {
+			message: 'Error finding questions'
+		});
+	}
 };
