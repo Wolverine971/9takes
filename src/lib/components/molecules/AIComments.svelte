7d69455fb89e2423e0d0317ab0d212c795a284f7
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2024-09-06 15:39:34 -0400

    updates

--- src/lib/components/molecules/AIComments.svelte
+++ src/lib/components/molecules/AIComments.svelte
@@ -158,14 +158,9 @@
 	}
 
 	.carousel-item {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
+		margin: auto;
+		height: 80%;
+		width: 80%;
 	}
 
 	.arrow {
