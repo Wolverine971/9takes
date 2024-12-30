560e7168071ab1e6b003e8036a21d9ecd4f31f14
david.t.wayne <djwayne35@gmail.com>
2024-12-29 21:18:36 -0500

    updates

--- src/routes/blog/enneagram/subtopic/[slug]/+page.server.ts
+++ src/routes/blog/enneagram/subtopic/[slug]/+page.server.ts
@@ -1,28 +1,9 @@
 import type { PageServerLoad } from './$types';
-import { slugFromPath } from '$lib/slugFromPath';
+import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({
-	url,
 	params
-}): Promise<{ posts: App.BlogPost[]; slug: string }> => {
-	const slug = params.slug;
-
-	const modules = import.meta.glob(`/src/blog/enneagram/*.{md,svx,svelte.md}`);
-
-	const postPromises = Object.entries(modules).map(([path, resolver]) =>
-		resolver().then(
-			(post) =>
-				({
-					...(post as unknown as App.MdsvexFile).metadata,
-					slug: slugFromPath(path)
-				}) as App.BlogPost
-		)
-	);
-
-	const posts = await Promise.all(postPromises);
-	const publishedPosts = posts.filter((post) => post.published && post.type?.[0] === slug); //.slice(0, MAX_POSTS);
-
-	publishedPosts.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
-
-	return { posts: publishedPosts, slug };
+}) => {
+	throw redirect(302, '/enneagram-corner/subtopic/' + params.slug);
 };
+
