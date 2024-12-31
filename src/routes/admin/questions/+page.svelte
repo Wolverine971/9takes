ed24a428fff1b23220715bdb78cf6aa5dd25c4e3
david.t.wayne <djwayne35@gmail.com>
2024-12-30 20:29:30 -0500

    updates

--- src/routes/admin/questions/+page.svelte
+++ src/routes/admin/questions/+page.svelte
@@ -4,6 +4,9 @@
 	import { invalidateAll } from '$app/navigation';
 	import { Button, Tabs, TabItem } from 'flowbite-svelte';
 
+	import Modal2, { getModal } from '$lib/components/atoms/Modal2.svelte';
+	import { convertDateToReadable } from '../../../utils/conversions';
+	import { onMount } from 'svelte';
 	export let data: PageData;
 
 	const navItems = [
@@ -15,6 +18,57 @@
 		{ href: '/links', label: 'Links' },
 		{ href: '/admin/messages', label: 'Messages' }
 	];
+	// console.log(data);
+
+	const normalSort = (questions) => {
+		return questions.sort((a, b) => {
+			console.log(questions);
+			// Convert last_comment_date to a timestamp, treating null as zero
+			const dateA = a.last_comment_date ? new Date(a.last_comment_date).getTime() : 0;
+			const dateB = b.last_comment_date ? new Date(b.last_comment_date).getTime() : 0;
+
+			// First, compare by last_comment_date descending
+			if (dateB !== dateA) {
+				return dateB - dateA;
+			}
+
+			// If dates are the same or both are null, compare by comment_count descending
+			return b.comment_count - a.comment_count;
+		});
+	};
+
+	const newestSort = (questions) => {
+		console.log(questions);
+		return questions.sort((a, b) => {
+			// Convert last_comment_date to a timestamp, treating null as zero
+			const dateA = a.created_at ? new Date(a.created_at).getTime() : 0;
+			const dateB = b.created_at ? new Date(b.created_at).getTime() : 0;
+
+			return dateB - dateA;
+		});
+	};
+	const oldestSort = (questions) => {
+		console.log(questions);
+		return questions.sort((a, b) => {
+			// Convert last_comment_date to a timestamp, treating null as zero
+			const dateA = a.created_at ? new Date(a.created_at).getTime() : 0;
+			const dateB = b.created_at ? new Date(b.created_at).getTime() : 0;
+
+			return dateA - dateB;
+		});
+	};
+
+	let selectedQuestion = null;
+	let displayedQuestions = [];
+	const openModal = (questionData) => {
+		console.log(questionData);
+		selectedQuestion = questionData;
+		getModal(`quetion-details-modal`).open();
+	};
+
+	onMount(() => {
+		displayedQuestions = normalSort(data.questions);
+	});
 </script>
 
 <svelte:head>
@@ -37,15 +91,52 @@
 			<Button href="/admin/questions/hierarchy">View Hierarchy</Button>
 		</div>
 
-		{#if data?.questions?.length}
+		<div class="my-6 flex w-full gap-2">
+			<Button
+				on:click={() => {
+					displayedQuestions = normalSort(data.questions);
+				}}>Last Comment</Button
+			>
+			<Button
+				on:click={() => {
+					displayedQuestions = newestSort(data.questions);
+				}}>Newest</Button
+			>
+			<Button
+				on:click={() => {
+					displayedQuestions = oldestSort(data.questions);
+				}}>Oldest</Button
+			>
+		</div>
+
+		{#if displayedQuestions.length}
 			<div class="rounded-lg bg-white p-4 shadow-md dark:bg-gray-800">
 				<div class="-mr-4 max-h-[calc(100vh-200px)] space-y-4 overflow-y-auto pr-4">
-					{#each data.questions as questionData}
-						<AdminQuestionItem
-							{questionData}
-							tags={data.tags || []}
-							on:questionRemoved={() => invalidateAll()}
-						/>
+					{#each displayedQuestions as questionData}
+						<div
+							style="border-radius: 5px;
+    padding: 0 .5rem; background-color: {questionData.removed
+								? '#f9cfcf'
+								: questionData.flagged
+									? '#ffffd4'
+									: ''};"
+						>
+							<h2 class="text-xl font-bold text-gray-800 dark:text-gray-200">
+								{questionData.question_formatted || questionData.question}
+							</h2>
+							<div class="flex items-center justify-between">
+								<p class="text-gray-600 dark:text-gray-400">
+									Comments #: {questionData.comment_count}, Created at: {convertDateToReadable(
+										questionData.created_at
+									)}, Last Comment: {convertDateToReadable(questionData.last_comment_date)}
+								</p>
+								<button
+									type="button"
+									style="border-radius: 5px; border: 1px solid grey; padding: 0.25rem 0.5rem;"
+									on:click={() => openModal(questionData)}>Details</button
+								>
+							</div>
+						</div>
 					{/each}
 				</div>
 			</div>
@@ -61,3 +152,15 @@
 		</p>
 	</div>
 {/if}
+
+<Modal2 id="quetion-details-modal">
+	<div class="modal-content">
+		{#if selectedQuestion}
+			<AdminQuestionItem
+				questionData={selectedQuestion}
+				tags={data.tags || []}
+				on:questionRemoved={() => invalidateAll()}
+			/>
+		{/if}
+	</div>
+</Modal2>
