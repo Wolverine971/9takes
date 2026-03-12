45858d4bde2a95392543a27d5eef31734d54d138
DJ Wayne <djwayne35@gmail.com>
2026-03-11 21:42:35 -0400

    updates

--- src/routes/api/questions/typeahead/+server.ts
+++ src/routes/api/questions/typeahead/+server.ts
@@ -3,6 +3,16 @@ import { json } from '@sveltejs/kit';
 import { elasticClient } from '$lib/server/elasticSearch';
 import type { RequestHandler } from './$types';
 
+/** Strip all HTML except <em> tags used by Elasticsearch highlights */
+function sanitizeHighlight(html: string): string {
+	// Temporarily replace ES highlight tags with placeholders
+	let safe = html.replace(/<em>/gi, '\x00EM_OPEN\x00').replace(/<\/em>/gi, '\x00EM_CLOSE\x00');
+	// Escape all remaining HTML
+	safe = safe.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	// Restore highlight tags
+	return safe.replace(/\x00EM_OPEN\x00/g, '<em>').replace(/\x00EM_CLOSE\x00/g, '</em>');
+}
+
 export const GET: RequestHandler = async ({ url, locals }) => {
 	// Add authentication check
 	if (!locals.session?.user?.id) {
@@ -67,7 +77,7 @@ export const GET: RequestHandler = async ({ url, locals }) => {
 			question: hit._source.question,
 			url: hit._source.url,
 			comment_count: hit._source.comment_count || 0,
-			highlighted: hit.highlight?.question?.[0] || hit._source.question
+			highlighted: sanitizeHighlight(hit.highlight?.question?.[0] || hit._source.question)
 		}));
 
 		return json({ results });
@@ -142,7 +152,7 @@ export const POST: RequestHandler = async ({ request, locals }) => {
 			question: hit._source.question,
 			url: hit._source.url,
 			comment_count: hit._source.comment_count || 0,
-			highlighted: hit.highlight?.question?.[0] || hit._source.question
+			highlighted: sanitizeHighlight(hit.highlight?.question?.[0] || hit._source.question)
 		}));
 
 		return json({ results });
